ce66ff8a618374ef7db390a317539d194d457aaf
Kharanshu Wanare <kharanshuw@gmail.com>
2025-01-20 13:54:43 +0530

    user has to use his key

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@
 
 export const getCurrency = async (from,to) => {
 
-    let url = `https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_RUWtpIFaMCTdl3NmPqtYws99FCPVTb94S8064IZk&currencies=${to}&base_currency=${from}`;
+    let url = `https://api.freecurrencyapi.com/v1/latest?apikey=${key}&currencies=${to}&base_currency=${from}`;
 
     const response = await fetch(url,
         {
